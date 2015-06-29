errantzebra845/packages_apps_Settings

[Settings] Improve HeadsUp
Change-Id: I927cf171978a49d93c62c038a31d6e4046cc9ed0

    5.1.1

commit 3e2e13576ee281c93e4fa3b54efb3f3ab6f23577 1 parent 0705e78
@BigBrother1984 BigBrother1984 authored on May 8Add heads settings activity to manifest

Change-Id: Ie983b3907739827e0df7679576fba956e3499295
Signed-off-by: mar5hal <errantzebra845@gmail.com>

Conflicts:
	AndroidManifest.xml
	src/com/android/settings/Settings.java
	src/com/android/settings/SettingsActivity.java

Conflicts:
	AndroidManifest.xml
	src/com/android/settings/SettingsActivity.java

 Author:    cristianomatos <cristianobmatos@gmail.com>

src/com/android/settings/SettingsActivity.java
@@ -81,6 +81,7 @@ import com.android.settings.applications.ProcessStatsUi;
 import com.android.settings.blacklist.BlacklistSettings;
 import com.android.settings.bluetooth.BluetoothSettings;
 import com.android.settings.cyanogenmod.DisplayRotation;
+import com.android.settings.candy.HeadsUpSettings;
 import com.android.settings.dashboard.DashboardCategory;
 import com.android.settings.dashboard.DashboardSummary;
 import com.android.settings.dashboard.DashboardTile;
@@ -126,7 +127,6 @@ import com.android.settings.wifi.AdvancedWifiSettings;
 import com.android.settings.wifi.SavedAccessPointsWifiSettings;
 import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
-import com.android.settings.candy.HeadsUpSettings;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;

MinimalOS/android_packages_apps_Settings

src/com/android/settings/SettingsActivity.java
@@ -77,6 +77,7 @@ import com.android.settings.applications.ManageApplications;
 import com.android.settings.applications.ProcessStatsUi;
 import com.android.settings.blacklist.BlacklistSettings;
 import com.android.settings.bluetooth.BluetoothSettings;
+import com.android.settings.crdroid.HeadsUpSettings;
 import com.android.settings.dashboard.DashboardCategory;
 import com.android.settings.dashboard.DashboardSummary;
 import com.android.settings.dashboard.DashboardTile;
@@ -120,7 +121,6 @@ import com.android.settings.wifi.AdvancedWifiSettings;
 import com.android.settings.wifi.SavedAccessPointsWifiSettings;
 import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
-import com.android.settings.crdroid.HeadsUpSettings;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;