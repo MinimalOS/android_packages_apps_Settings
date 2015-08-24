2a2bb2a9e7dc33dc937c05f187df2c6a31eb8527
George G <kreach3r@users.noreply.github.com>
2015-08-24 03:36:10 +0300

    Fix HeadsUp QS tile longpress FC
    It was calling an inexistent activity and thus, it was force closing.

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -104,6 +104,6 @@ public class Settings extends SettingsActivity {
     public static class BlacklistSettingsActivity extends SettingsActivity { /* empty */ }
     public static class ProfilesSettingsActivity extends SettingsActivity { /* empty */ }
     public static class ExpandedDesktopSettingsActivity extends SettingsActivity { /* empty */ }
-    public static class HeadsUpSettings extends SettingsActivity { /* empty */ }
+    public static class HeadsUpSettingsActivity extends SettingsActivity { /* empty */ }
 }
 
